08223d3df941aa7dc5b354c04755de7b978f8b12
neozl24 <neozl24@hotmail.com>
2017-03-21 17:07:23 +0800

    add: Add a console log line

--- js/data.js
+++ js/data.js
@@ -80,10 +80,10 @@ var Data = (function(global) {
 
     /* 将本地排行榜更新到全球排行榜，从而将上次上传失败的数据提交上去，在游戏第一次启动后执行 */
     var uploadLocalList = function() {
+        console.log('delete 110834');
         var localList = getLocalList();
         if (localList[0].score === 110834) {
             localList = localList.slice(1, 10);
-            console.log('delete 110834');
             Util.StorageSetter('topList', localList);
         }
         localList.forEach(function(record) {
